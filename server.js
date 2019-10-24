a21ae733415d2cfa6e92e05e96918c17fa60c23f
Nathan Heller <naheller@gmail.com>
2019-10-23 21:58:21 -0700

    Add Podcast and Episode types

--- server.js
+++ server.js
@@ -6,13 +6,58 @@ const axios = require('axios')
 const baseUrl = 'https://listen-api.listennotes.com/api/v2'
 
 const schema = buildSchema(`
+  type Podcast {
+    description_highlighted: String
+    description_original: String
+    earliest_pub_date_ms: Int
+    email: String
+    explicit_content: Boolean
+    genre_ids: [Int]
+    id: String
+    image: String
+    itunes_id: Int
+    latest_pub_date_ms: Int
+    listennotes_url: String
+    publisher_highlighted: String
+    publisher_original: String
+    thumbnail: String
+    title_highlighted: String
+    title_original: String
+    total_episodes: Int
+  }
+  type Episode {
+    audio: String
+    audio_length_sec: Int
+    description_highlighted: String
+    description_original: String
+    explicit_content: Boolean
+    genre_ids: [Int]
+    id: String
+    image: String
+    itunes_id: Int
+    listennotes_url: String
+    podcast_id: String
+    podcast_listennotes_url: String
+    podcast_title_highlighted: String
+    podcast_title_original: String
+    publisher_highlighted: String
+    publisher_original: String
+    pub_date_ms: Int
+    rss: String
+    thumbnail: String
+    title_highlighted: String
+    title_original: String
+    total_episodes: Int
+    transcripts_highlighted: [String]
+  }
   type Query {
-    audioUrl(podcastName: String!): String
+    episodesByName(name: String!): [Episode]
+    podcastsByName(name: String!): [Podcast]
   }
 `)
 
-const getPodcastAudioUrl = ({ podcastName }) => {
-  const formattedName = podcastName.trim()
+const getEpisodesByName = ({ name }) => {
+  const formattedName = name.trim()
 
   return axios
     .get(`${baseUrl}/search`, {
@@ -20,15 +65,34 @@ const getPodcastAudioUrl = ({ podcastName }) => {
         'X-ListenAPI-Key': process.env.LISTEN_API_KEY
       },
       params: {
-        q: formattedName
+        q: formattedName,
+        type: 'episode'
       }
     })
-    .then(res => res.data.results[0].audio)
+    .then(res => res.data.results)
+    .catch(err => console.log(err))
+}
+
+const getPodcastsByName = ({ name }) => {
+  const formattedName = name.trim()
+
+  return axios
+    .get(`${baseUrl}/search`, {
+      headers: {
+        'X-ListenAPI-Key': process.env.LISTEN_API_KEY
+      },
+      params: {
+        q: formattedName,
+        type: 'podcast'
+      }
+    })
+    .then(res => res.data.results)
     .catch(err => console.log(err))
 }
 
 const root = {
-  audioUrl: getPodcastAudioUrl
+  episodesByName: getEpisodesByName,
+  podcastsByName: getPodcastsByName
 }
 
 const app = express()
